1e0980ca38e227ee5e318eaabad0eeb208eb797d
fukuhara0114 <fukuhara0114@gmail.com>
2024-03-23 23:49:12 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,8 @@
+<?php
+
+$myArray = ['name'=>'opinion', 'price'=>200, 'weight'=> 160];
+
+print_r("Array([name]=>{$myArray['name']}[opinion]=>{$myArray['price']}[weight]=>{$myArray['weight']}");
+
+
+?>
