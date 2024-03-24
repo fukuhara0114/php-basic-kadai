eca6d9f843823421037f2ea8011308490d12d01a
fukuhara0114 <55003576+fukuhara0114@users.noreply.github.com>
2024-03-25 06:56:04 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+      class Food{
+        private $name;
+        private $price;
+
+        public function __construct($name,$price){
+          $this->name = $name;
+          $this->price = $price;
+        }
+
+        public function show_price(){
+          echo $this->price.'円<br>';
+        }
+      }
+
+      class Animal{
+        private $weight;
+        private $height;
+
+        public function __construct($weight,$height){
+          $this->weight = $weight;  
+          $this->height = $height;  
+          
+          }
+
+          public function show_height(){
+            echo $this->height.'Kg<br>';
+          }
+      }
+
+      $food = new Food('たまねぎ', 200);
+      $food->show_price();
+
+      $animal = new Animal(180,300);
+      $animal->show_height();
+
+    ?>
+  </p>
+  
+</body>
+</html>
